746e1965346a94bbe345cc4996fd6cc568e0f860
nothing36 <prodipsarker953@gmail.com>
2025-11-02 12:10:47 +0600

    Student component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-    <p className="text-3xl">Prodip</p>
+    <h2 className="text-center text-2xl font-bold p-5">Information form</h2>
+    <Student name="Prodip" dist="Tangail"></Student>
+    <Student name="Roman" dist="Mymensingh"></Student>
+    <Student name="Momen" dist="Ghatail"></Student>
     </>
   )
 }
+function Student(person){
+  console.log(person);
+  return (
+    <div className="justify-center  p-5 space-y-2 m-5 rounded-lg w-50 mx-auto bg-gray-300">
+      <h3>Name:{person.name}</h3>
+      <h3>Dist:{person.dist}</h3>
+    </div>
+    
+  )
+}
+
 
 export default App
