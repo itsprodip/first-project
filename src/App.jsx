d9e623e57b3ec687abe52e84768d1ee903c1a133
nothing36 <prodipsarker953@gmail.com>
2025-11-03 10:27:57 +0600

    object destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ function App() {
 
   return (
     <>
-    <h2 className="text-center text-2xl font-bold p-5">Information form</h2>
-    <Student name="Prodip" dist="Tangail"></Student>
-    <Student name="Roman" dist="Mymensingh"></Student>
-    <Student name="Momen" dist="Ghatail"></Student>
+      <h2 className="text-center text-2xl font-bold p-5">Information form</h2>
+      <Student name="Prodip" dist="Tangail"></Student>
+      <Student name="Roman" dist="Mymensingh"></Student>
+      <Student name="Momen" dist="Ghatail"></Student>
+      <Student name="Abc" dist="Dhaka"></Student>
+
+      <h2 className="text-center text-2xl font-bold p-5">Player Information</h2>
+      <Player name="Rohit" runs="1250"></Player>
+      <Player name="Kohli" runs="1130"></Player>
     </>
-  )
+  );
 }
 function Student(person){
   console.log(person);
@@ -18,6 +23,15 @@ function Student(person){
     </div>
     
   )
+}
+
+function Player({name,runs}){
+  return (
+    <div className="justify-center  p-5 space-y-2 m-5 rounded-lg w-50 mx-auto bg-gray-300">
+      <h3>Name:{name}</h3>
+      <h3>Runs:{runs}</h3>
+    </div>
+  );
 }
 
 
